53e4f01b815504ddb1343a87e963984b2d286252
mj061203 <mjgarrett3.14@gmail.com>
2021-01-06 15:49:06 +0900

    mj first commit

---
+++ Assets/CameraMove.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+
+    public Transform player;
+    public Vector3 P_Pos;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.position + P_Pos;
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody r3D;
+    float jumpower = 0;
+    bool jumped = false;
+    float myYpos;
+    public Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myYpos = transform.position.y;
+        r3D = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+
+        if(Input.GetKey(KeyCode.D)){
+            transform.Translate(0,0,0.3f);
+        } if (Input.GetKey(KeyCode.A)){
+            transform.Translate(0,0,-0.3f);
+        } if (Input.GetKey(KeyCode.W)){
+            transform.Translate(-0.3f,0,0);
+        } if (Input.GetKey(KeyCode.S)){
+            transform.Translate(0.3f,0,0);
+
+        } if ( Input.GetKeyDown(KeyCode.Space) && player.position ){
+            
+            r3D.AddForce(transform.up * 480);
+
+        }
+
+        
+
+    }
+}
